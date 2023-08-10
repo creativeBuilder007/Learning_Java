5dd7007c359d959a6858759fda798762eea2bc59
creativeBuilder007 <3pm2022adityaprakash@gmail.com>
2023-08-10 07:41:37 +0530

    New Triangle Pattern Added

--- Pattern.java
+++ Pattern.java
@@ -15,6 +15,17 @@ class Pattern {
         //     System.out.println();
         // }
         // 2 - Triangle Pattern
+        int n=11;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(i<=5 && i+j>=5 && j-i<=5) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
         
     }
 }
