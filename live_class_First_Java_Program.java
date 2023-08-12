fa3713462e5a3d6045827dd66e9a1f41ecbaf989
creativeBuilder007 <3pm2022adityaprakash@gmail.com>
2023-08-12 14:54:56 +0530

    Learning about how classes and objects are created

---
+++ live_class_First_Java_Program.java
@@ -0,0 +1,37 @@
+class Fan {
+    String brand;
+    String color;
+    int no_of_blades;
+    int price;
+
+    void Starting() {
+        System.out.println("Fan is Starting");
+    }
+    void Rotating() {
+        System.out.print("Fan is Rotating ");
+    }
+    void BlowingAir() {
+        System.out.printf("Fan is Blowing Air ");
+    }
+    void Stopping() {
+        System.out.println("Fan is Stopping");
+    }
+
+    public static void main(String[] args) {
+        Fan f1 = new Fan();
+        f1.Starting();
+        f1.Stopping();
+        Fan f2 = new Fan();
+        f2.Starting();
+        f2.Rotating();
+        Fan f3 = new Fan();
+        f3.Starting();
+        f3.Rotating();
+        f3.BlowingAir();
+        f3.Stopping();
+
+        System.out.println(f1);
+        System.out.println(f3);
+        System.out.println(f2);
+    }
+}
