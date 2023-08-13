d08464acd51e744b6c173fb21222be990e05222a
creativeBuilder007 <3pm2022adityaprakash@gmail.com>
2023-08-13 11:23:34 +0530

    Added new pattern

--- Pattern.java
+++ Pattern.java
@@ -1,5 +1,6 @@
+import java.util.*;
 class Pattern {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         // 1 - Pallindrome Pattern
         // int n = 4;
         // for (int i=1;i<=n;i++) {
@@ -15,17 +16,50 @@ class Pattern {
         //     System.out.println();
         // }
         // 2 - Triangle Pattern
-        int n=11;
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                if(i<=5 && i+j>=5 && j-i<=5) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
-        }
-        
+        // int n=11;
+        // for(int i=0;i<n;i++) {
+        //     for(int j=0;j<n;j++) {
+        //         if(i<=5 && i+j>=5 && j-i<=5) {
+        //             System.out.print("* ");
+        //         } else {
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        // 3 - Reverse Number Triangle Pattern
+        // Scanner scan = new Scanner(System.in);
+        // int n = scan.nextInt();
+        // for(int i=0;i<n;i++) {
+        //     for(int j=i;j>0;j--) {
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=i+1;j<=n;j++) {
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+        // 4 - Mirror Image Triangle Pattern
+        // Scanner scan = new Scanner(System.in);
+        // int n = scan.nextInt();
+        // for(int i=0;i<n;i++) {
+        //     for(int j=i;j>0;j--) {
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=i+1;j<=n;j++) {
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }    
+        // for(int i=n-2;i>=0;i--) {
+        //     for(int j=i;j>0;j--) {
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=i+1;j<=n;j++) {
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();    
+       // }
+
     }
 }
